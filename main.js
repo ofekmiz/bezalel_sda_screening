6cc63b7dcb7f26401d160696f1e5173a23c29366
OfeX <ofekkm@gmail.com>
2022-07-04 15:17:48 +0300

    movie list js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-var dataJson = CSVJSON.csv2json(DATA);
-console.log(dataJson);
+document.addEventListener("DOMContentLoaded", function () {
+    var dataJson = CSVJSON.csv2json(DATA);
+    console.log("DATA",dataJson);
 
+    var movieListContainer = document.getElementById("movieList");
+
+    //append movies to list
+    for (var i = 1; i < dataJson.length; i++) {
+        let authors = dataJson[i].authors;
+        let movieName = dataJson[i].hebMovieName;
+
+        let movie = document.createElement("div");
+        movie.classList.add("movie");
+        movie.innerHTML = /*html*/ `
+        <b class="autors">${authors}</b> | <span class="movieName">${movieName}</span>
+    `
+        movieListContainer.appendChild(movie);
+    }
+});
